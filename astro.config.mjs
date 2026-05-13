1b21098275cf94de285d3cce40fc99a89c374228
insciobg <bgallagher@inscio.com>
2026-05-13 09:13:25 -0500

    Remove sitemap integration (version compatibility fix)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://inscio.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+    site: 'https://inscio.com',
+    integrations: [
+          tailwind(),
+        ],
 });
